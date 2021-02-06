936a1e0aad6a998c994532d8e55903c543e17bb2
Vlad Aleksieiev <vladyslav.aleksieiev@gmail.com>
2021-02-06 11:44:45 +0200

    feat: add mongo db connection

---
+++ src/database/setup.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import { connect } from 'mongoose';
+
+export const setupDatabase = async (): Promise<void> => {
+  config();
+  try {
+    await connect(process.env.DB_CONNECTION_ATLAS, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB CONNECTED');
+  } catch (e) {
+    console.log('DB CONNECTION ERROR ', e);
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import { setupDatabase } from '@database/setup';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  setupDatabase();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
